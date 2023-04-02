cffe8f47a901f407c60b8167249d4e0a089aaef9
DaemoniumLupus <37290179+DaemoniumLupus@users.noreply.github.com>
2023-04-02 19:38:01 +0300

    SSSSSSSSSSSSSSSSSSSSSSSSS

---
+++ Skillbox/12/series of number/series_of_number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int repetition(int number[]){
+
+    
+}
+
+int main(){
+    int number[15] = {114, 111, 106, 107, 108, 105, 115, 108,
+                  110, 109, 112, 113, 116, 117, 118};
+    int repetition();
+    cout << "Repeating number"
+
+}
+
+
+    
