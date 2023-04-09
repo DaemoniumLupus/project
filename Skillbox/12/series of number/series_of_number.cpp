abec4cfd058603fba43a4bced4896f50168de7eb
DaemoniumLupus <37290179+DaemoniumLupus@users.noreply.github.com>
2023-04-09 17:06:28 +0300

    new

--- Skillbox/12/series of number/series_of_number.cpp
+++ Skillbox/12/series of number/series_of_number.cpp
@@ -2,15 +2,18 @@
 
 using namespace std;
 
-int repetition(int number[]){
-
-    
-}
+int repetition(int number[]) { 
+    int summ;
+    for (int i = 0; i < 15; i++){
+      summ += number[i];
+      
+    }
+ }
 
 int main(){
     int number[15] = {114, 111, 106, 107, 108, 105, 115, 108,
                   110, 109, 112, 113, 116, 117, 118};
-    int repetition();
+    int repetition(number[15]);
     cout << "Repeating number";
 }
 
