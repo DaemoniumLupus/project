aba28c24d3f368501b340b41b2640183866eb8a5
DaemoniumLupus <37290179+DaemoniumLupus@users.noreply.github.com>
2023-04-23 20:39:28 +0300

    new

--- Skillbox/15/3.cpp
+++ Skillbox/15/3.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include <vector>
 
-
-
-
 int ascending_order(std::vector<int> vec) {
   int max = 0;
   int max_ind = 0;
+  /*  for (int i = 1; i < vec.size(); i++) {
+     for (int j = 1; j < vec.size(); j++)
+     { */
   for (int i = 1; i < vec.size(); i++) {
-    for (int j = 1; j < vec.size(); j++)
-    {
-        
-    
-    
-      if (vec[i] > vec[i-1]) {
+    for (int j = i; j > 0 && vec[j - 1] > vec[j];
+         j--) { // пока j>0 и элемент j-1 > j, x-массив int
+      int temp;
+      temp = vec[j - 1];
+      vec[j - 1] = vec[j];
+      vec[j] = temp;
+
+      /* if (vec[i] > vec[i - 1]) {
         int temp;
         temp = vec[i - 1];
         vec[i - 1] = vec[i];
         vec[i] = temp;
-      }
-    
+      } */
     }
   }
   std::cout << std::endl;
@@ -39,13 +40,23 @@ int main() {
 
   while (n != -2) {
     n = 0;
+    vec.clear();
     while (n != -1) {
       std::cin >> n;
-      if (n != -1 || n != -2) {
-        vec.push_back(n);
+      if (n == -2) {
+        break;
       }
+      vec.push_back(n);
+      
     }
+    
     vec.pop_back();
-    std::cout << ascending_order(vec);
+    if (vec.size() >= 5 ) {
+      
+      std::cout << ascending_order(vec);
+      
+    }else{
+      std::cerr << "insufficient array size" << std::endl;
+    }
   }
 }
