fa7f8a45b60363e681d6588f82185aa7cb8dd386
DaemoniumLupus <37290179+DaemoniumLupus@users.noreply.github.com>
2023-05-05 21:18:11 +0300

    new

--- Skillbox/test/file.cpp
+++ Skillbox/test/file.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 #include <fstream> // работа с файлами
+int main() {
+  std::ifstream bank;
+  bank.open("D:\\Directory\\project\\Skillbox\\test\\bank.txt");//открытие файла
+  int money;
+  bank >> money;
+  bank >> money;
+  std::cout << money << std::endl;
+  bank.close();//закрытие файла
+
+
+}
