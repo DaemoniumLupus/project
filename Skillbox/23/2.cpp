ad7024bb1c25aaad6614c10ec42c190cc68a1e55
Ilya Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-06-15 15:59:48 +0300

    Add task 2

--- Skillbox/23/2.cpp
+++ Skillbox/23/2.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 
-#define 
+#define SPRING 0
+#define SUMMER 0
+#define AUTUMN 0
+#define WINTER 0
+#define COUT_SPRING std::cout << "SPRING!" << std::endl;
+#define COUT_SUMMER std::cout << "SUMMER!" << std::endl;
+#define COUT_AUTUMN std::cout << "AUTUMN!" << std::endl;
+#define COUT_WINTER std::cout << "WINTER!" << std::endl;
+
+
+#if SPRING
+int main() {COUT_SPRING }
+#endif
+#if SUMMER
+int main() {COUT_SUMMER }
+#endif
+#if AUTUMN
+int main() {COUT_AUTUMN }
+#endif
+#if WINTER
+int main() {COUT_WINTER }
+#endif
